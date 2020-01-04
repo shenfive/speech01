c3c502373f813417bb9170cefe3eb686eb06c06b
Danny Shen <shenfive@gmail.com>
2020-01-04 14:36:39 +0800

    Initial Commit

---
+++ speech01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  speech01
+//
+//  Created by 申潤五 on 2020/1/4.
+//  Copyright © 2020 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
